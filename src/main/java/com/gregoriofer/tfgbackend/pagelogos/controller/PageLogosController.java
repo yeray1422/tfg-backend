668943fe511be9bc50065d95c95b99608a212202
yeray1422 <yeray14sj@gmail.com>
2022-07-28 10:51:35 +0200

    removed unused import

--- src/main/java/com/gregoriofer/tfgbackend/pagelogos/controller/PageLogosController.java
+++ src/main/java/com/gregoriofer/tfgbackend/pagelogos/controller/PageLogosController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-// import org.json.simple.JSONArray;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
